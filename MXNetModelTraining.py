2e4c56dc21696ad0bababe705be6894a0e770eb4
striderssoftware <stider@shadowsoftware.biz>
2023-11-07 19:37:07 -0700

    remove code so this is only the creation and training of a model

--- MXNetModelTraining.py
+++ MXNetModelTraining.py
@@ -1,4 +1,4 @@
-pip install MXNet
+#!pip install MXNet
 
 import mxnet as mnet
 from mxnet import nd
@@ -13,32 +13,9 @@ import time
 print ("strider was here", mnet.__version__)
 
 
-#  Get Data
-mnist_train = datasets.FashionMNIST(train=True)
-X, y = mnist_train[0]
-('X shape: ', X.shape, 'X dtype', X.dtype, 'y:', y)
-
-
-
-text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
-               'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
-
-
-X, y = mnist_train[0:10]
-# plot images
-_, figs = plt.subplots(1, X.shape[0], figsize=(15, 15))
-
-for f,x,yi in zip(figs, X,y):
-    # 3D->2D by removing the last channel dim
-    f.imshow(x.reshape((28,28)).asnumpy())
-    ax = f.axes
-    ax.set_title(text_labels[int(yi)])
-    ax.title.set_fontsize(14)
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
-
 #  Get Training Data
+mnist_train = datasets.FashionMNIST(train=True)
+
 transformer = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(0.13, 0.31)])
@@ -48,21 +25,14 @@ batch_size = 256
 train_data = gluon.data.DataLoader(
         mnist_train, batch_size=batch_size, shuffle=True, num_workers=4)
 
-for data, label in train_data:
-        print(data.shape, label.shape)
-        break
-
 mnist_valid = gluon.data.vision.FashionMNIST(train=False)
 valid_data = gluon.data.DataLoader(
         mnist_valid.transform_first(transformer),
         batch_size=batch_size, num_workers=4)
 
 
-#  Create a sequential model
-
+#  Create a Model  # Add the layers explictly
 net = nn.Sequential()
-
-#  Add the layers explictly
 net.add(nn.Conv2D(channels=6, kernel_size=5, activation='relu'),
                         nn.MaxPool2D(pool_size=2, strides=2),
                         nn.Conv2D(channels=16, kernel_size=3, activation='relu'),
@@ -71,18 +41,12 @@ net.add(nn.Conv2D(channels=6, kernel_size=5, activation='relu'),
                         nn.Dense(84, activation="relu"),
                         nn.Dense(10))
 
-
 net.initialize()
-# Input shape is (batch_size, color_channels, height, width)
-x = nd.random.uniform(shape=(4,1,28,28))
-y = net(x)
-y.shape
 
 softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
 trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.1})
 
-
-#  Train the Model
+# Check ouput with label
 def acc(output, label):
     # output: (batch, num_output) float32 ndarray
     # label: (batch, ) int32 ndarray
@@ -90,7 +54,7 @@ def acc(output, label):
             label.astype('float32')).mean().asscalar()
 
 
-
+#  Train the Model
 for epoch in range(2):
     train_loss, train_acc, valid_acc = 0., 0., 0.
     tic = time.time()
@@ -111,10 +75,8 @@ for epoch in range(2):
     print("Epoch %d: loss %.3f, train acc %.3f, test acc %.3f, in %.1f sec" % (
         epoch, train_loss/len(train_data), train_acc/len(train_data),
         valid_acc/len(valid_data), time.time()-tic))
-
-
     
 net.save_parameters('net.params')
 
-print ("all doneeeeeeefffffffe")
+print ("all doneefe")
 
