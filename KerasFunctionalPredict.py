46fd4a66ecde5265cd342ea8de2f59bbd75544aa
striderssoftware <stider@shadowsoftware.biz>
2023-11-11 01:45:26 -0700

    Create 4 models using the Functinal Graph.

--- KerasFunctionalPredict.py
+++ KerasFunctionalPredict.py
@@ -23,8 +23,16 @@ x = layers.Dense(256, activation="relu")(x)
 x = layers.Dropout(0.5)(x)
 outputs = layers.Dense(10)(x)
 
+
+# define some models using the Functional Graph
 model = keras.Model(inputs, outputs, name="toy_resnet")
 #model.summary()
+
+block1Model = keras.Model(inputs, block_1_output, name="toy_resnetb1")
+
+block2Model = keras.Model(block_1_output, block_2_output, name="toy_resnetb2")
+
+block3Model = keras.Model(block_2_output, block_3_output, name="toy_resnetb3")
 
 # Train
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -34,14 +42,28 @@ x_test = x_test.astype("float32") / 255.0
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
+
+# compile some of the Models
 model.compile(
         optimizer=keras.optimizers.RMSprop(1e-3),
         loss=keras.losses.CategoricalCrossentropy(from_logits=True),
         metrics=["acc"],
     )
-# We restrict the data to the first 1000 samples so as to limit execution time
-# on Colab. Try to train on the entire dataset until convergence!
+
+
+block1Model.compile(
+        optimizer=keras.optimizers.RMSprop(1e-3),
+        loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+        metrics=["acc"],
+    )
+
+
+# Call fit on some of the Models Note: no training data for some
 model.fit(x_train[:1000], y_train[:1000], batch_size=64, epochs=1, validation_split=0.2)
+
+# Cannot call fit on this Model ONLY because there is no Training Data that has
+# the right output size,
+#block1Model.fit(x_train[:1000], y_train[:1000], batch_size=64, epochs=1, validation_split=0.2)
 
 # Predict
 output = model.predict(x_test[:1])
@@ -51,6 +73,6 @@ print (output)
 
 print ("strider was here: ENDEEeeeee")
 
-plot_model(model, show_shapes=True, show_layer_names=True, to_file='model.png')
+plot_model(block1Model, show_shapes=True, show_layer_names=True, to_file='model.png')
 
 
