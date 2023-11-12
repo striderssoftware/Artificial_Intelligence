4c3b30d0c01be830b01e234e4553c84cfdbd1f83
striderssoftware <stider@shadowsoftware.biz>
2023-11-11 21:33:27 -0700

    Add better commenting, fix some typos

--- KerasFunctionalPredict.py
+++ KerasFunctionalPredict.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 
 print ("strider was here")
 
+#  Define a Functional Graph of layers
+
 inputs = keras.Input(shape=(32, 32, 3), name="img")
 x = layers.Conv2D(32, 3, activation="relu")(inputs)
 x = layers.Conv2D(64, 3, activation="relu")(x)
@@ -25,7 +27,7 @@ x = layers.Dropout(0.5)(x)
 outputs = layers.Dense(10)(x)
 
 
-# define some models using the Functional Graph
+#  Define some models using the Functional Graph
 model = keras.Model(inputs, outputs, name="toy_resnet")
 #model.summary()
 
@@ -35,18 +37,18 @@ block2Model = keras.Model(block_1_output, block_2_output, name="toy_resnetb2")
 
 block3Model = keras.Model(block_2_output, block_3_output, name="toy_resnetb3")
 
-block4Model = keras.Model(block_3_output, outputs, name="toy_resnetb3")
+block4Model = keras.Model(block_3_output, outputs, name="toy_resnetb4")
 
-# Train
+
+#  Retreive and prepare Training Data
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-
 x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32") / 255.0
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
 
-# compile some of the Models
+#  Compile the Models
 model.compile(
         optimizer=keras.optimizers.RMSprop(1e-3),
         loss=keras.losses.CategoricalCrossentropy(from_logits=True),
@@ -79,14 +81,14 @@ block4Model.compile(
         )
 
 
-# Call fit on some of the Models Note: no training data for some
+# Call fit on the Models. Note: no training data for some
 model.fit(x_train[:1000], y_train[:1000], batch_size=64, epochs=1, validation_split=0.2)
 
-# Cannot call fit on this Model ONLY because there is no Training Data that has
-# the right output size,
+# Cannot call fit on the Block Models ONLY because there is no Training Data that has
+# the right data/output size,
 #block1Model.fit(x_train[:1000], y_train[:1000], batch_size=64, epochs=1, validation_split=0.2)
 
-# Predict
+# Run Predictions/Processing
 modelOutput = model.predict(x_test[:1])
 
 output = block1Model(x_test[:1])
@@ -94,6 +96,8 @@ output = block2Model(output)
 output = block3Model(output)
 output = block4Model(output)
 
+#  Check that the Complete Model and the Block Models
+#  produce the same output.
 areEqual = tf.math.equal(modelOutput, output)
 print (areEqual)
  
