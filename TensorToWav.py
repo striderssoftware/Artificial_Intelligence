7db6ecb36826be197ec4de0b501203dea11559de
Dennis Lucero <strider@shadowsoftware.biz>
2023-12-06 03:27:33 -0700

    Update TensorToWav.py

--- TensorToWav.py
+++ TensorToWav.py
@@ -2,7 +2,7 @@
 channels = 2  # 1 = mono 2 = stereo
 length = 200
 wavTensor = tf.zeros(shape=[length,channels], dtype="float32")
-sampleRateTensor = 44.1
+sampleRateTensor = 1600
 
 wavString = tf.audio.encode_wav(wavTensor, sampleRateTensor)
 
