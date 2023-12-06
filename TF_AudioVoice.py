c6bf7cf5e2309b8c3f4c19e310a7320714138f6f
striderssoftware <stider@shadowsoftware.biz>
2023-12-05 23:51:58 -0700

    TensorFlow Audio Model, Woo Woo

---
+++ TF_AudioVoice.py
@@ -0,0 +1,184 @@
+!pip install -U -q tensorflow tensorflow_datasets
+
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+
+print ("strider was here")
+
+# Set the seed value for experiment reproducibility.
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+DATASET_PATH = 'data/mini_speech_commands'
+
+data_dir = pathlib.Path(DATASET_PATH)
+if not data_dir.exists():
+    tf.keras.utils.get_file(
+    'mini_speech_commands.zip',
+    origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
+    extract=True,
+    cache_dir='.', cache_subdir='data')
+
+commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+commands = commands[(commands != 'README.md') & (commands != '.DS_Store')]
+print('Commands:', commands)
+
+train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
+    directory=data_dir,
+    batch_size=64,
+    validation_split=0.2,
+    seed=0,
+    output_sequence_length=16000,
+    subset='both')
+
+
+label_names = np.array(train_ds.class_names)
+
+train_ds.element_spec
+
+def squeeze(audio, labels):
+    audio = tf.squeeze(audio, axis=-1)
+    return audio, labels
+
+train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
+val_ds = val_ds.map(squeeze, tf.data.AUTOTUNE)
+
+test_ds = val_ds.shard(num_shards=2, index=0)
+val_ds = val_ds.shard(num_shards=2, index=1)
+
+for example_audio, example_labels in train_ds.take(1):
+    print(example_audio.shape)
+    print(example_labels.shape)
+
+def get_spectrogram(waveform):
+    # Convert the waveform to a spectrogram via a STFT.
+    spectrogram = tf.signal.stft(
+        waveform, frame_length=255, frame_step=128)
+    # Obtain the magnitude of the STFT.
+    spectrogram = tf.abs(spectrogram)
+    # Add a `channels` dimension, so that the spectrogram can be used
+    # as image-like input data with convolution layers (which expect
+    # shape (`batch_size`, `height`, `width`, `channels`).
+    spectrogram = spectrogram[..., tf.newaxis]
+    return spectrogram
+
+
+# here is a WTF: These vars are used later after this display loop
+# (side effect) straight from the example, hmmmm 
+for i in range(3):
+    label = label_names[example_labels[i]]
+    waveform = example_audio[i]
+    spectrogram = get_spectrogram(waveform)
+
+    #print('Label:', label)
+    #print('Waveform shape:', waveform.shape)
+    #print('Spectrogram shape:', spectrogram.shape)
+    #print('Audio playback')
+    #display.display(display.Audio(waveform, rate=16000))
+
+
+def make_spec_ds(ds):
+    return ds.map(
+        map_func=lambda audio,label: (get_spectrogram(audio), label),
+        num_parallel_calls=tf.data.AUTOTUNE)
+
+train_spectrogram_ds = make_spec_ds(train_ds)
+val_spectrogram_ds = make_spec_ds(val_ds)
+test_spectrogram_ds = make_spec_ds(test_ds)
+
+for example_spectrograms, example_spect_labels in train_spectrogram_ds.take(1):
+      break
+
+# Build and Train
+train_spectrogram_ds = train_spectrogram_ds.cache().shuffle(10000).prefetch(tf.data.AUTOTUNE)
+val_spectrogram_ds = val_spectrogram_ds.cache().prefetch(tf.data.AUTOTUNE)
+test_spectrogram_ds = test_spectrogram_ds.cache().prefetch(tf.data.AUTOTUNE)
+
+input_shape = example_spectrograms.shape[1:]
+print('Input shape:', input_shape)
+num_labels = len(label_names)
+
+# Instantiate the `tf.keras.layers.Normalization` layer.
+norm_layer = layers.Normalization()
+# Fit the state of the layer to the spectrograms
+# with `Normalization.adapt`.
+norm_layer.adapt(data=train_spectrogram_ds.map(map_func=lambda spec, label: spec))
+
+model = models.Sequential([
+    layers.Input(shape=input_shape),
+    # Downsample the input.
+    layers.Resizing(32, 32),
+    # Normalize.
+    norm_layer,
+    layers.Conv2D(32, 3, activation='relu'),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.25),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(num_labels),
+])
+
+#model.summary()
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+)
+
+EPOCHS = 1  #10
+history = model.fit(
+    train_spectrogram_ds,
+    validation_data=val_spectrogram_ds,
+    epochs=EPOCHS,
+    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
+)
+
+# Evaluate the Model
+model.evaluate(test_spectrogram_ds, return_dict=True)
+
+y_pred = model.predict(test_spectrogram_ds)
+y_pred = tf.argmax(y_pred, axis=1)
+y_true = tf.concat(list(test_spectrogram_ds.map(lambda s,lab: lab)), axis=0)
+
+confusion_mtx = tf.math.confusion_matrix(y_true, y_pred)
+plt.figure(figsize=(10, 8))
+sns.heatmap(confusion_mtx,
+            xticklabels=label_names,
+            yticklabels=label_names,
+            annot=True, fmt='g')
+plt.xlabel('Prediction')
+plt.ylabel('Label')
+plt.show()
+
+
+x = data_dir/'no/01bb6a2a_nohash_0.wav'
+x = tf.io.read_file(str(x))
+x, sample_rate = tf.audio.decode_wav(x, desired_channels=1, desired_samples=16000,)
+x = tf.squeeze(x, axis=-1)
+waveform = x
+x = get_spectrogram(x)
+x = x[tf.newaxis,...]
+
+prediction = model(x)
+x_labels = ['no', 'yes', 'down', 'go', 'left', 'up', 'right', 'stop']
+plt.bar(x_labels, tf.nn.softmax(prediction[0]))
+plt.title('No')
+plt.show()
+
+display.display(display.Audio(waveform, rate=16000))
+
+
+print ("Endeeeeeeeeeeeee")
